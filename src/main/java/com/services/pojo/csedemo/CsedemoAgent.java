3ac75c6874fbfebbdc88e8aef0c77836b541f43f
lesonalmao <33471133+lesonalmao@users.noreply.github.com>
2017-11-08 09:43:29 +0800

    Update CsedemoAgent.java

--- src/main/java/com/services/pojo/csedemo/CsedemoAgent.java
+++ src/main/java/com/services/pojo/csedemo/CsedemoAgent.java
@@ -15,7 +15,7 @@ public class CsedemoAgent {
     public String sayHei(String name){
 
         // Do Some Magic Here!
-        return "hi, " + name + "! welcome to ServiceStage";
+        return null;
     };
 
 
@@ -29,7 +29,7 @@ public class CsedemoAgent {
     public String sayHi(String name){
 
         // Do Some Magic Here!
-        return null;
+        return "hi, " + name + "! welcome to ServiceStage";
     };
 
 
